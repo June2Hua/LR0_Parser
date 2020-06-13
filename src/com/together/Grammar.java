e95187303e759138c43dc2081165f499fbe2ff82
June2Hua <827259596@qq.com>
2020-06-13 15:26:31 +0800

    修改部分错误

--- src/com/together/Grammar.java
+++ src/com/together/Grammar.java
@@ -5,10 +5,10 @@ package com.together;
  */
 public class Grammar {
 
-    //文法终结符，包括了“#”，并且“E”放在vt数组的最后
+    //文法终结符，包括了“#”，并且“#”放在vt数组的最后
     private char[] vt;
 
-    //文法非终结符，第一个非终结符为开始符
+    //文法非终结符，并且第一个非终结符为开始符
     private char[] vn;
 
     //文法产生式,如“E->E+T”、“F->(E)”等等   产生式中不存在如“E->E+T| i”的形式，即不存在“ | ”
